5e9605984e10f976d49e498e5b3cef09b15d0592
Ochieng Kamau <ochiengkamauj@gmail.com>
2023-04-04 19:27:56 +0300

    [Update:] Removed unnecessary whitespace

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -12,4 +12,3 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     ''' Return an asyncio.Task object '''
     return asyncio.create_task(wait_random(max_delay))
-
